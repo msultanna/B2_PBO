3ebe416669d767e4147306bb26a7f5ce855f8673
ismiatunnisa <ismiatunnisa1234@gmail.com>
2022-03-29 19:20:47 +0800

    Saya melengkapi codingan saya

--- 2009106136_ismiatunnisa/src/pkg2009106136_ismiatunnisa/Main.java
+++ 2009106136_ismiatunnisa/src/pkg2009106136_ismiatunnisa/Main.java
@@ -5,76 +5,242 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 public class Main {
-    static ArrayList ListStudyClub = new ArrayList();
+    static ArrayList StudyClubWeb = new ArrayList();
+    static ArrayList StudyClubNet = new ArrayList();
+    static ArrayList StudyClubRbt = new ArrayList();
     static boolean isRunning = true;
     static InputStreamReader inputStreamReader = new InputStreamReader(System.in);
     static BufferedReader input = new BufferedReader(inputStreamReader);
 
     static void ShowMenu() throws IOException{
-        System.out.println("======Menu=====");
-        System.out.println("[1] Show All Study Club");
-        System.out.println("[2] Tambah Study Club");
-        System.out.println("[3] Edit Study Club");
-        System.out.println("[4] Hapus Study Club");
-        System.out.println("[5] Exit");
+        System.out.println("======Menu Study Club=====");
+        System.out.println("[1] WEB PROGRAMMING");
+        System.out.println("[2] NETWORK");
+        System.out.println("[3] ROBOTIC");
+        System.out.println("[0] Exit");
         System.out.println("Pilih menu>");
         
         int PilihMenu = Integer.valueOf(input.readLine());
         switch(PilihMenu){
             case 1:
-                showAllStudyClub();
+                WebProgramming();
                 break;
             case 2:
-                TambahStudyClub();
+                Network();
                 break;
             case 3:
-                EditStudyClub();
+                Robotic();
                 break;
             case 4:
-                HapusStudyClub();
-                break;
-            case 5:
                 System.exit(0);
                 break;
             default:
                 System.out.println("Pilihan anda tidak tersedia");
         }
     }
-        static void showAllStudyClub(){
-            if(ListStudyClub.isEmpty()){
+        static void WebProgramming(){
+            System.out.println("======Menu WEB PROGRAMMING=====");
+            System.out.println("[1] Lihat Pendaftar");
+            System.out.println("[2] Tambah Pendaftar");
+            System.out.println("[3] Edit Pendaftar");
+            System.out.println("[4] Hapus Pendaftar");
+            System.out.println("[0] Back to Menu Study Club");
+            System.out.println("Pilih menu>");
+            
+            int Pilih = Integer.valueOf(input.readLine());
+            switch(Pilih){
+                case 1:
+                    LihatWeb();
+                    break;
+                case 2:
+                    TambahWeb();
+                    break;
+                case 3:
+                    EditWeb();
+                    break;
+                case 4:
+                    HapusWeb();
+                    break;
+                case 5:
+                    ShowMenu();
+                    break;
+                default:
+                    System.out.println("Pilihan anda tidak tersedia");
+               }
+        }
+        static void LihatWeb() throws IOException{
+            if(StudyClubWeb.isEmpty()){
                 System.out.println("Belum ada data");
             } else {
-                for(int i = 0; i < ListStudyClub.size(); i++){
-                    System.out.println(String.format("[%d] %s",i, ListStudyClub.get(i)));
+                for(int i = 0; i < StudyClubWeb.size(); i++){
+                    System.out.println(String.format("[%d] %s",i, StudyClubWeb.get(i)));
                 }
-            }     
+            }
+            WebProgramming();
         }
-        static void TambahStudyClub() throws IOException{
-            System.out.println("Nama Study Club:");
+        static void TambahWeb() throws IOException{
+            System.out.println("Nama Lengkap:");
             String namastudyclub = input.readLine();
-            ListStudyClub.add(namastudyclub);
+            StudyClubWeb.add(namastudyclub);
+            WebProgramming();
         }
-        static void EditStudyClub() throws IOException{
-            showAllStudyClub();
-            System.out.print("Pilih nomor Study Club:");
+        static void EditWeb() throws IOException{
+            LihatWeb();
+            System.out.print("Pilih nomor nama pendaftar Study Club:");
             int indexStudyClub = Integer.valueOf(input.readLine());
             
-            System.out.println("Nama Study Club Baru:");
+            System.out.println("Nama Pendaftar Baru:");
             String namaBaru = input.readLine();
             
-            ListStudyClub.set(indexStudyClub, namaBaru);
+            StudyClubWeb.set(indexStudyClub, namaBaru);
+            WebProgramming();
         }
-        static void HapusStudyClub() throws IOException{
-            showAllStudyClub();
-            System.out.print("Pilih nomer Study Club Yang Ingin dihapus:");
+        static void HapusWeb() throws IOException{
+            LihatWeb();
+            System.out.print("Pilih nomer nama pendaftar Yang Ingin dihapus:");
             int indexStudyClub = Integer.valueOf(input.readLine());
             
-            ListStudyClub.remove(indexStudyClub);
+            StudyClubWeb.remove(indexStudyClub);
+            WebProgramming();
         }
+        static void Network(){
+            System.out.println("======Menu Network=====");
+            System.out.println("[1] Lihat Pendaftar");
+            System.out.println("[2] Tambah Pendaftar");
+            System.out.println("[3] Edit Pendaftar");
+            System.out.println("[4] Hapus Pendaftar");
+            System.out.println("[0] Back to Menu Study Club");
+            System.out.println("Pilih menu>");
+            
+            int Pilih = Integer.valueOf(input.readLine());
+            switch(Pilih){
+                case 1:
+                    LihatNet();
+                    break;
+                case 2:
+                    TambahNet();
+                    break;
+                case 3:
+                    EditWeb();
+                    break;
+                case 4:
+                    HapusNet();
+                    break;
+                case 5:
+                    ShowMenu();
+                    break;
+                default:
+                    System.out.println("Pilihan anda tidak tersedia");
+               }
+        }
+        static void LihatNet() throws IOException{
+            if(StudyClubNet.isEmpty()){
+                System.out.println("Belum ada data");
+            } else {
+                for(int i = 0; i < StudyClubNet.size(); i++){
+                    System.out.println(String.format("[%d] %s",i, StudyClubNet.get(i)));
+                }
+            }
+            Network();
+        }
+        static void TambahNet() throws IOException{
+            System.out.println("Nama Lengkap:");
+            String namastudyclub = input.readLine();
+            StudyClubNet.add(namastudyclub);
+            Network();
+        }
+        static void EditNet() throws IOException{
+            LihatWeb();
+            System.out.print("Pilih nomor nama pendaftar Study Club:");
+            int indexStudyClub = Integer.valueOf(input.readLine());
+            
+            System.out.println("Nama Pendaftar Baru:");
+            String namaBaru = input.readLine();
+            
+            StudyClubNet.set(indexStudyClub, namaBaru);
+            Network();
+        }
+        static void HapusNet() throws IOException{
+            LihatWeb();
+            System.out.print("Pilih nomer nama pendaftar Yang Ingin dihapus:");
+            int indexStudyClub = Integer.valueOf(input.readLine());
+            
+            StudyClubNet.remove(indexStudyClub);
+            Network();
+        }
+        
+        static void Robotic(){
+            System.out.println("======Menu Robotic=====");
+            System.out.println("[1] Lihat Pendaftar");
+            System.out.println("[2] Tambah Pendaftar");
+            System.out.println("[3] Edit Pendaftar");
+            System.out.println("[4] Hapus Pendaftar");
+            System.out.println("[0] Back to Menu Study Club");
+            System.out.println("Pilih menu>");
+            
+            int Pilih = Integer.valueOf(input.readLine());
+            switch(Pilih){
+                case 1:
+                    LihatRbt();
+                    break;
+                case 2:
+                    TambahRbt();
+                    break;
+                case 3:
+                    EditRbt();
+                    break;
+                case 4:
+                    HapusRbt();
+                    break;
+                case 5:
+                    ShowMenu();
+                    break;
+                default:
+                    System.out.println("Pilihan anda tidak tersedia");
+               }
+        }
+        static void LihatRbt() throws IOException{
+            if(StudyClubRbt.isEmpty()){
+                System.out.println("Belum ada data");
+            } else {
+                for(int i = 0; i < StudyClubRbt.size(); i++){
+                    System.out.println(String.format("[%d] %s",i, StudyClubRbt.get(i)));
+                }
+            }
+            Robotic();
+        }
+        static void TambahRbt() throws IOException{
+            System.out.println("Nama Lengkap:");
+            String namastudyclub = input.readLine();
+            StudyClubRbt.add(namastudyclub);
+            Robotic();
+        }
+        static void EditRbt() throws IOException{
+            LihatWeb();
+            System.out.print("Pilih nomor nama pendaftar Study Club:");
+            int indexStudyClub = Integer.valueOf(input.readLine());
+            
+            System.out.println("Nama Pendaftar Baru:");
+            String namaBaru = input.readLine();
+            
+            StudyClubRbt.set(indexStudyClub, namaBaru);
+            Robotic();
+        }
+        static void HapusRbt() throws IOException{
+            LihatWeb();
+            System.out.print("Pilih nomer nama pendaftar Yang Ingin dihapus:");
+            int indexStudyClub = Integer.valueOf(input.readLine());
+            
+            StudyClubRbt.remove(indexStudyClub);
+            Robotic();
+        }
+        
         public static void main(String[] args)throws IOException{
             do{
                 ShowMenu();
             }while (isRunning);
         }
     }
+
+
 
