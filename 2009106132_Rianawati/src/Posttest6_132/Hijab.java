97a008b579541b57a05baf2f9dfbb5cde060b675
Rianawati <rianawati180402@gmail.com>
2022-05-18 20:51:38 +0800

    Posttest6_2009106132

--- 2009106132_Rianawati/src/Posttest6_132/Hijab.java
+++ 2009106132_Rianawati/src/Posttest6_132/Hijab.java
@@ -3,21 +3,26 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Posttest5_132;
+package Posttest6_132;
+
+import Posttest5_132.*;
+import Posttest4_132.*;
 
 /**
  *
  * @author Lenovo Gk
  */
-final public class NonHijab extends Pakaian {
-    int diskon;
+final class Hijab extends Pakaian {
+    private int biaya_tambah;
+    private int diskon;
 
-    public NonHijab(String nama, String bahan, String warna, int harga, int jumlah) {
+    public Hijab(String nama, String bahan, String warna, int harga, int jumlah, int diskon) {
         super(nama, bahan, warna, harga, jumlah);
         this.diskon = 30;
-    }
+        }
+
     
-        void BerhasilPesan() {
+    void BerhasilPesan() {
         System.out.println("\n*================================*");
         System.out.println("| Nama            : " + this.nama);
         System.out.println("| Bahan           : " + this.bahan);
@@ -37,9 +42,9 @@ final public class NonHijab extends Pakaian {
         System.out.println("| Harga           : " + this.harga);
         System.out.println("| Jumlah          : " + this.jumlah);
         System.out.println("*================================*");
-        System.out.println("|        Berhasil Dibatalkan      |");
+        System.out.println("|        Berhasil Dibatal        |");
         System.out.println("*================================*\n");
-    }
+    }                      
     
     void UbahPesanan() {
         System.out.println("\n*================================*");
@@ -49,16 +54,17 @@ final public class NonHijab extends Pakaian {
         System.out.println("| Harga           : " + this.harga);
         System.out.println("| Jumlah          : " + this.jumlah);
         System.out.println("*================================*");
-        System.out.println("|       Berhasil Ditetapkan      |");
+        System.out.println("|        Berhasil Diubah         |");
         System.out.println("*================================*\n");
     }
     
     public void overloading(int diskon_harga){
         final int diskon_tambahan = 10;
+        System.out.println("| Diskon Yang Didapat : " + this.diskon);
         diskon_harga = this.harga * this.diskon/100;
         System.out.println("| Total Biaya    : \n| \t\t" + diskon_harga + " Rupiah");
         System.out.println("*================================*\n");
-        
+                           
     }
     
 }
--- 2009106132_Rianawati/src/Posttest6_132/NonHijab.java
+++ 2009106132_Rianawati/src/Posttest6_132/NonHijab.java
@@ -3,7 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Posttest5_132;
+package Posttest6_132;
+
+import Posttest5_132.*;
 
 /**
  *
