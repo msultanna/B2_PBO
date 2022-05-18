f95eeb2023fb4f22198161c16f550f912456be7a
AlyaniNS <septaliaans@gmail.com>
2022-05-18 21:16:53 +0800

    Revert "POSTTEST6"
    This reverts commit f951bbab93c497c88bada3492d16d76fde480123.

--- 2009106100_Alyani Noor Septalia/src/PostTest6/InheritanceReservasi.java
+++
@@ -1,9 +0,0 @@
-package PostTest6;
-
-public abstract class InheritanceReservasi {
-    public abstract void statusHapusReservasi();
-
-    public void hapusReservasi() {
-        System.out.println("Berhasil dihapus");
-    }
-}
